32238726e9c5d734add8e904a91ac08b3b63a4fa
Vinay Datta <pvinaydatta@gmail.com>
2019-04-01 02:05:47 +0000

    Add the ipython notebooks

---
+++ data_download.sh
@@ -0,0 +1,9 @@
+wget https://challenge.kitware.com/api/v1/item/5ac37a9d56357d4ff856e176/download 
+mv download train.zip
+unzip train.zip
+wget https://challenge.kitware.com/api/v1/item/5ac3695656357d4ff856e16a/download
+mv download groundtruth.zip
+unzip groundtruth.zip
+wget https://challenge.kitware.com/api/v1/item/5ae1d97856357d4ff8570ca0/download
+mv download groundtruth_task2.zip
+unzip groundtruth_task2.zip
